bb9c15b0b092b02643ffbc46ad2d4f9034815de4
leozhang2018 <leozhang2018@gmail.com>
2021-11-25 21:48:19 +0800

    Change user login page into dex.
    Signed-off-by: leozhang2018 <leozhang2018@gmail.com>

---
+++ custom-web/static/js/main.js
@@ -0,0 +1,63 @@
+const hostname = window.location.hostname;
+const protocol = window.location.protocol;
+
+// login();
+var dex = new Vue({
+    el: "#dex-login",
+    delimiters: ["{{{", "}}}"],
+    data: {
+        loginForm: {
+            account: "",
+            password: "",
+        },
+        showRegistration:false,
+        showLoginError:false
+    },
+    methods: {
+        async login() {
+            const payload = this.loginForm;
+            try {
+                const userInfoRes = await axios.post("/api/v1/login", payload);
+                const userInfo = userInfoRes.data;
+                if (userInfo) {
+                    store.set("userInfo", userInfo);
+                    const roleBindingRes = await axios.get(
+                        `/api/v1/userbindings?uid=${userInfo.uid}`,
+                        {
+                            headers: {
+                                Authorization: `Bearer ${userInfo.token}`,
+                            },
+                        }
+                    );
+                    const roleBinding = roleBindingRes.data;
+                    if (roleBinding) {
+                        const role = roleBinding.map((item) => item.role);
+                        store.set("role", role);
+                        window.location.href = `${protocol}//${hostname}/v1/projects`;
+                    }
+                }
+            } 
+            catch (error) {
+                this.showLoginError = true;
+            }
+
+        },
+        findPassword() {
+            window.location.href = `${protocol}//${hostname}/signin?findPassword=true`;
+        },
+        signUp() {
+            window.location.href = `${protocol}//${hostname}/signin?signUp=true`;
+        },
+        async registrationCheck(){
+            const registrationRes = await axios.get(`/api/v1/features/RegisterTrigger`);
+            if (registrationRes.data && registrationRes.data.enabled) {
+                this.showRegistration = true
+              } else {
+                this.showRegistration = false
+              }
+        }
+    },
+    mounted() {
+        this.registrationCheck();
+    },
+});
